10c46f1c42b401933acb9d2429aa9a9356070a4f
ChristieL12 <chritieluo@gmail.com>
2022-06-26 13:38:16 -0700

    Added my phone number

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("Hello World");
 		System.out.println("12345");
+		System.out.println("My Phone Number is 12345678");
 	}
 }
